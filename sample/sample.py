cde17229c6d0d009480d908d7a9d06194d34fffd
benschau <bchau3440@gmail.com>
2017-01-16 22:55:08 -0500

    arg parsing now works with default argument

--- sample/sample.py
+++ sample/sample.py
@@ -18,13 +18,15 @@ client = ImgurClient(absclient.id, absclient.secret)
 # TODO: parse commandline args -> -c compress -t custom_title -i id-only
 # get link from user (via argument)
 parser = argparse.ArgumentParser(prog='imgurdl')
-parser.add_argument('-c', '--compress', nargs='?', help="compress downloaded gallery")
+parser.add_argument('link')
+parser.add_argument('-c', '--compress', action='store_true', help="compress downloaded gallery")
 parser.add_argument('-t', '--title', nargs='?', help="rename downloaded gallery")
 parser.add_argument('-i', '--id-only', nargs='?', help="input gallery id instead of whole link")
 
-# args = parser.parse_args(sys.argv[1:])
+# TODO: If given a link, use the link. If given an ID, use the ID. Don't allow a user to give both arguments.
+args = parser.parse_args()
 
-link = str(sys.argv[1])
+link = args.link
 
 # Strip everything in front of imgur
 ind = link.find('imgur')
