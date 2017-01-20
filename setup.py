a87b8a73e3f6bb20213d618a899ed19e62114efa
benschau <bchau3440@gmail.com>
2017-01-19 21:35:18 -0500

    added metadata setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2.7
+
+from distutils.core import setup
+
+setup(
+    name='imgurdl',
+    version='0.1dev',
+    packages=['imgurdl',],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
+
+
