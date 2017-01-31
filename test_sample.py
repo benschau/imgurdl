9d7df389d9b732ab806149672cb76eb6c44b1c3a
benschau <bchau3440@gmail.com>
2017-01-31 00:07:41 -0500

    added client credential testing

--- test_sample.py
+++ test_sample.py
@@ -1,11 +1,11 @@
 import pytest
 from sample.client import Client
 
-
-
-"""
 def test_clientinit():
     client = Client()
-    assert client.id == 
-    assert client.secret == 
-"""
+    
+    cpath = expanduser('~/.imgur_cred') 
+    imgurcred = [line.rstrip('\n') for line in open(truepath)] 
+
+    assert client.id == imgurcred[0]
+    assert client.secret == imgurcred[1]
