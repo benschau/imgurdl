e09e6fc7d039268d609867519bb1d6b96d49cd9b
benschau <bchau3440@gmail.com>
2017-02-07 22:43:52 -0500

    fixed title argument issues

--- sample/sample.py
+++ sample/sample.py
@@ -13,32 +13,32 @@ import argparse
 # import helper
 from client import Client
 
-parser = argparse.ArgumentParser(prog='imgurdl')
-parser.add_argument('id')
-parser.add_argument('-c', '--compress', action='store_true', help="compress downloaded gallery")
-parser.add_argument('-t', '--title', nargs=1, help="rename downloaded gallery")
-parser.add_argument('-cp', '--credpath', nargs=1, help="define custom credential path")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='imgurdl')
+    parser.add_argument('id')
+    parser.add_argument('-c', '--compress', action='store_true', help="compress downloaded gallery")
+    parser.add_argument('-t', '--title', nargs=1, help="rename downloaded gallery")
+    parser.add_argument('-cp', '--credpath', nargs=1, help="define custom credential path")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-absclient = Client(args.credpath)
-client = absclient.client
-# client = ImgurClient(absclient.id, absclient.secret)
+    absclient = Client(args.credpath)
+    client = absclient.client
 
-album_id = args.id
+    album_id = args.id
 
-album = client.get_album(album_id)
-album_title = args.title if args.title else album.title
+    album = client.get_album(album_id)
+    album_title = args.title[0] if args.title[0] else album.title[0]
 
-if not os.path.exists(album_title):
-    os.mkdir(album_title)
+    if not os.path.exists(album_title):
+        os.mkdir(album_title)
 
-os.chdir(album_title)
+    os.chdir(album_title)
 
-album_images = client.get_album_images(album_id)
-for image in album_images:
-    image_title = image.link[(image.link.rindex('/') + 1):]
-    print('\t{0}: {1}'.format(image_title, image.link))
-    urllib.urlretrieve(image.link, image_title) 
+    album_images = client.get_album_images(album_id)
+    for image in album_images:
+        image_title = image.link[(image.link.rindex('/') + 1):]
+        print('\t{0}: {1}'.format(image_title, image.link))
+        urllib.urlretrieve(image.link, image_title) 
 
-os.chdir('../')
+    os.chdir('../')
